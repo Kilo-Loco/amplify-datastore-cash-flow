93d69db76d5323ac47139f283f9ed312c5fcb265
Kilo Loco <kiloloco@me.com>
2020-06-17 07:59:38 -0700

    Initial commit

---
+++ Cash-Flow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cash-Flow
+//
+//  Created by Kyle Lee on 6/17/20.
+//  Copyright © 2020 Kilo Loco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
